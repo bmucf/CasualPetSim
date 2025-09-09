759f553c3dd213f97a471fe786b67369d1e717cc
bmucf <br407069@ucf.edu>
2025-09-09 00:02:46 -0400

    a

--- Assets/Scripts/PetDefinition.cs
+++ Assets/Scripts/PetDefinition.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class PetDefinition : MonoBehaviour
 {
-    public string petName;
-    public string petSpecies;
-
+    //declaring basic needs of pet
     public float foodLevel;
     public float hungerRate;
-    public float waterLevel;
+
+    public float hydrationLevel;
     public float thirstRate;
+
     public float restLevel;
-    public float tireRate;
+    public float exhaustRate;
 
     public float size;
+    public float growthRate;
+
+    private void Update()
+    {
+        Hunger();
+        Thirst();
+        Exhaust();
+    }
+
+    public void Hunger()
+    {
+        foodLevel -= hungerRate * Time.deltaTime;
+    }
+
+    public void Thirst()
+    {
+        hydrationLevel -= thirstRate * Time.deltaTime;
+    }
+
+    public void Exhaust()
+    {
+        restLevel -= exhaustRate * Time.deltaTime;
+    }
 }
